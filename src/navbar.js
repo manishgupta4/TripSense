893fc94ff53126be2c6873c530766ca55970a3b3
manish~ <manishumeshgupta3@gmail.com>
2024-09-04 22:32:52 +0530

    href

--- src/navbar.js
+++ src/navbar.js
@@ -1,18 +1,19 @@
 import './App.css';
+import image from './logo.png';
 function Nvbr() {
   return (
     <div className='row'>
         <nav className="navbar navbar-expand-sm ">
         <div className="container">
             <a className="navbar-brand mr-auto" href="index.html">
-                <img src="logo.png"  alt =" " id="logo"/>
+                <img src={image}  alt =" " id="logo"/>
                 TripSense
             </a>
                 <ul className="navbar-nav ml-auto">
-                    <li className="nav-item"> <a href="#" className=" nav-link">About Us</a></li>
-                    <li className="nav-item"> <a href="#" className=" nav-link">Blog </a></li>
-                    <li className="nav-item"><a href="#" className=" nav-link">Contact Us </a></li>
-                    <li className="nav-item"> <a href="#" className=" nav-link">FAQ </a></li>
+                    <li className="nav-item"> <a href="index.html" className=" nav-link">About Us</a></li>
+                    <li className="nav-item"> <a href="index.html" className=" nav-link">Blog </a></li>
+                    <li className="nav-item"><a href="index.html" className=" nav-link">Contact Us </a></li>
+                    <li className="nav-item"> <a href="index.html" className=" nav-link">FAQ </a></li>
                 </ul>
 
             </div>
