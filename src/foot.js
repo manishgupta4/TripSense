f9567802d114115fffa0b4c38c4b1122abcfa9e3
manish~ <manishumeshgupta3@gmail.com>
2024-09-05 22:43:58 +0530

    imgalt

--- src/foot.js
+++ src/foot.js
@@ -3,13 +3,13 @@ import image from './logo.png';
 const Foot = () => {
     return ( 
         <div className='row mt-5 mb-1 px-5 py-3 foot'>
-            <div className="col-sm-4 px-4">
+            <div className="col-sm-4 px-4 py-2 my-4">
                 <a className="navbar-brand mr-auto" href="index.html">
-                    <img src={image} id="logo"/>
+                    <img src={image} alt ="TS"id="logo"/>
                     Tripsense
                 </a>
             </div>
-            <div className="col-sm-4 px-4">
+            <div className="col-sm-4 px-4 py-2 my-4">
                 <ul className="navbar-nav ml-auto">
                     <li className="nav-item"> <a href="index.html" className="nav-link">About Us</a></li>
                     <li className="nav-item"> <a href="index.html" className="nav-link">Blogs </a></li>
@@ -17,7 +17,7 @@ const Foot = () => {
                     <li className="nav-item"> <a href="index.html" className="nav-link">FAQ </a></li>
                 </ul>
             </div>
-            <div className="col-sm-4 px-4">
+            <div className="col-sm-4 px-4 py-2 my-4">
               <ul className="navbar-nav ml-auto">
                 <li className="nav-item"><a href="http://google.com" ><i className="nav-link fa fa-google"></i>Google </a></li>
                 <li className="nav-item"><a href="http://www.facebook.com/profile.php?id=" ><i className="nav-link fa fa-facebook"></i>Facebook</a></li>
